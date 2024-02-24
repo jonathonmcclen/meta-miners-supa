86c801999c4692e663962c65ba0725feab7b88e4
--replace-all <jonathonsmcclendon@gmail.com>
2024-02-23 19:52:20 -0600

    push

--- src/Views/Splash/Splash.js
+++ src/Views/Splash/Splash.js
@@ -1,12 +1,13 @@
 function Splash() {
   return (
     <>
-      <div className="section flex px-[80px]">
-        <div className="w-2/3">
-          <img src="https://nrpcmqkzpwyhpqnxkftn.supabase.co/storage/v1/object/public/mics/dsadsdsfssdsd%201%20(2).png" />
-        </div>
-        <div className="p-[50px] w-2/3 border-2 border-[#22FC37]">
-          <h1 className="text-[200px]">Welcome To Meta Miners</h1>
+      <div className="section w-full max-h-[90vh] overflow-hidden static">
+        <img
+          className="h-[1000px] "
+          src="https://nrpcmqkzpwyhpqnxkftn.supabase.co/storage/v1/object/public/mics/dsadsdsfssdsd%201%20(2).png"
+        />
+        <div className="absolute p-[50px] border-2 border-[#22FC37]">
+          <h1 className="text-5xl">Welcome To Meta Miners</h1>
           <p>Start Mining Today!</p>
           <p>
             Are you ready for an exhilarating gaming experience? Look no further
@@ -22,57 +23,22 @@ function Splash() {
           </p>
         </div>
       </div>
+      <div className="bg-[#13131F]">pijpij</div>
       <div className="section flex">
-        <div className="w-1/3">
-          <img src="https://nrpcmqkzpwyhpqnxkftn.supabase.co/storage/v1/object/public/mics/Group%20244.png" />
+        <div className=" mx-auto">
+          <img
+            className="max-w-[350px]"
+            src="https://nrpcmqkzpwyhpqnxkftn.supabase.co/storage/v1/object/public/mics/Group%20244.png"
+          />
         </div>
-        <div className="p-[50px] w-2/3 border-2 border-[#22FC37]">
-          <h1 className="text-[200px]">Welcome To Meta Miners</h1>
-          <p>Start Mining Today!</p>
-          <p>
-            Are you ready for an exhilarating gaming experience? Look no further
-            than "MEAT MINERS"! This free-to-play game combines the excitement
-            of mining cryptocurrency with the thrill of collecting NFTs. Immerse
-            yourself in a simulated universe, where entire planets come to life
-            as the locals pay tribute to their creator. With over 200+ items to
-            collect from 5 obscure and unique worlds, the possibilities are
-            endless. Whether you're a seasoned gamer or a work-from-home
-            superstar looking for an idle game demo, "MEAT MINERS" offers an
-            adventure like no other. Get ready to dive into a world of endless
-            possibilities and become the ultimate crypto-collecting champion!
+        <div className="p-5 mr-[20%] my-auto">
+          <p className="text-7xl uppercase font-black tracking-[20px] leading-relaxed">
+            Something
+            <br /> about the
+            <br /> game
           </p>
         </div>
       </div>
-      <div></div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1728"
-        height="384"
-        fill="none"
-        viewBox="0 0 1728 384"
-      >
-        <g clip-path="url(#a)">
-          <path fill="#09F" d="m0 38.4 1728 230.4V384H0V38.4Z" />
-        </g>
-        <defs>
-          <clipPath id="a">
-            <path fill="#fff" d="M0 0h1728v384H0z" />
-          </clipPath>
-        </defs>
-      </svg>
-      <div>
-        <h3>Testing</h3>
-        <p>wecwvwvvwrw</p>
-      </div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1728"
-        height="355"
-        fill="none"
-        viewBox="0 0 1728 355"
-      >
-        <path fill="#09F" d="m0 40.8 1836 244.8V0H0v40.8Z" />
-      </svg>
     </>
   );
 }
