757bf703b54ee856eae1c988d02e6891ac0c9a4e
James <james.evanowski@gmail.com>
2024-02-23 20:38:10 -0600

    Responsive Splash

--- src/Views/Splash/Splash.js
+++ src/Views/Splash/Splash.js
@@ -1,17 +1,17 @@
 function Splash() {
   return (
     <>
-      <div className="section w-full h-screen max-h-[90vh] flex place-items-center">
+      <div className="mt-[100px] md:mt-0 section w-full overflow-hidden h-screen max-h-[90vh] flex gap-12 md:gap-0 flex-row place-items-center">
   
-        <div className="flex flex-col">
-          <h1 className="px-[50px] text-7xl uppercase font-thin tracking-widest">Do you want <br/>to get rich?</h1>
-          <div className="px-[50px] border-2 border-t-[#22FC37] border-r-[#22FC37] border-b-black border-l-black">
-          <p className="pt-4 font-black underline uppercase tracking-wide underline-offset-8 hover:underline-offset-[12px] hover:drop-shadow-[0_4px_21px_rgba(255,235,205,1)]  transition-all duration-300 cursor-pointer">Start Mining Today!</p>
+        <div className="w-2/3 md:w-1/2 flex flex-col place-items-end">
+          <h1 className="text-3xl md:text-5xl lg:text-7xl uppercase font-thin tracking-widest">Do you want <br/>to get rich?</h1>
+          <div className="w-full text-right px-6 py-4 border-2 border-t-[#22FC37] border-r-[#22FC37] border-b-black border-l-black">
+          <p className="pt-2 font-black underline uppercase tracking-wide underline-offset-8 hover:underline-offset-[12px] hover:drop-shadow-[0_4px_21px_rgba(255,235,205,1)]  transition-all duration-300 cursor-pointer">Start Mining Today!</p>
           </div>
         </div>
  
         <img
-          className="h-[70vh] inline-block"
+          className="rotate-180 md:rotate-0 h-[50vh] md:h-[70vh] -mr-[420px] inline-block"
           src="https://nrpcmqkzpwyhpqnxkftn.supabase.co/storage/v1/object/public/mics/dsadsdsfssdsd%201%20(2).png"
         />
       </div>
